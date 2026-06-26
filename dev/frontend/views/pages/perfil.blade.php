f32c38fae70ae58027ddb1e903064e069837bc9a
tecnologiaporan <tecnologia@porancosmeticos.com.br>
2026-06-26 14:23:57 -0300

    a

--- dev/frontend/views/pages/perfil.blade.php
+++ dev/frontend/views/pages/perfil.blade.php
@@ -81,16 +81,16 @@ if (!function_exists('formatarCpfCnpj')) {
                                 </td>
                                 <td class="cr-col-acoes">
                                     <button class="btn-acao btn-edit" id="btn-edit-<?= $b->ID_DETALHE ?>" onclick="editarBaixa(<?= $b->ID_DETALHE ?>)" title="Editar Baixa">
-                                        <x-icons.pencil width="16" height="16" />
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg>
                                     </button>
                                     <button class="btn-acao btn-save" id="btn-save-<?= $b->ID_DETALHE ?>" onclick="salvarBaixa(<?= $b->ID_DETALHE ?>)" title="Salvar" style="display:none; color: #16a34a;">
                                         <x-icons.check width="16" height="16" />
                                     </button>
                                     <button class="btn-acao btn-cancel" id="btn-cancel-<?= $b->ID_DETALHE ?>" onclick="cancelarEdicaoBaixa(<?= $b->ID_DETALHE ?>)" title="Cancelar" style="display:none; color: #64748b;">
-                                        <x-icons.x width="16" height="16" />
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
                                     </button>
                                     <button class="btn-acao btn-delete" onclick="estornarBaixa(<?= $b->ID_DETALHE ?>)" title="Estornar Baixa">
-                                        <x-icons.trash width="16" height="16" />
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
                                     </button>
                                 </td>
                             </tr>
